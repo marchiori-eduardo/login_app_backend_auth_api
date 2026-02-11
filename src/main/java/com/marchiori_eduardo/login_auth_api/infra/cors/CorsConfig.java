48a3e64d15cc78c4fbfd1331538ac04dace1ca4e
Eduardo Marchiori <marchiorimarchiori2@gmail.com>
2026-02-11 16:37:35 -0300

    Change domain: 'https://login-app-frontend-omega.vercel.app'

--- src/main/java/com/marchiori_eduardo/login_auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/marchiori_eduardo/login_auth_api/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://login-app-frontend-omega.vercel.app")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
